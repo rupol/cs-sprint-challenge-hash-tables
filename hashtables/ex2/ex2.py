8efcfb4e46da60a9de85a3c69d7c4a854a77f058
Ruth Poliakon <r.poliakon@gmail.com>
2020-08-16 17:44:39 -0600

    ex2 completed - tests passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,4 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
+
+# input: list of tickets, each with source and destination attributes
+# output: array of strings with the correct ordered route
 class Ticket:
     def __init__(self, source, destination):
         self.source = source
@@ -6,9 +9,33 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # store tickets in dict
+    # key: ticket source
+    # value: ticket destination
+    ticket_dict = {ticket.source: ticket.destination for ticket in tickets}
+    route = [None] * length
+    # the first ticket will have a source of "NONE"
+    route[0] = ticket_dict["NONE"]
+    # for the remaining tickets, route[i] = ticket_dict[i-1]
+    for i in range(length - 1):
+        route[i+1] = ticket_dict[route[i]]
 
     return route
+
+
+# test
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+           ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+print(reconstruct_trip(tickets, 10))
