eeb181c389f3f2019a822ad2230bb0f7687c40fb
Ruth Poliakon <r.poliakon@gmail.com>
2020-08-16 17:23:41 -0600

    ex1 completed - tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,33 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+# inputs: two lists
+# limit
+# weights (list) - item weights
 
+# output
+# tuple - two items (a, b) where:
+# weights[a] + weights[b] = limit
+# a is the higher value
+# if no pair exists, return None
+
+def get_indices_of_item_weights(weights, length, limit):
+    # memoization - store each weight in dict with
+    # key: weight (from weight list)
+    # value: weight index
+    weights_dict = {weight: index for index, weight in enumerate(weights)}
+    # loop through weights
+    for index, weight in enumerate(weights):
+        # weight_2 is the weight which combines with the current weight to equal the limit
+        weight_2 = limit - weight
+        # if it exists
+        if weight_2 in weights_dict:
+            # return the indexes of the two weights in specified order
+            indexes_list = [index, weights_dict[weight_2]]
+            indexes_list.sort(reverse=True)
+            return indexes_list
     return None
+
+
+weights = [4, 6, 10, 15, 16]
+length = 5
+limit = 21
+
+print(get_indices_of_item_weights(weights, length, limit))
